23c4453439c2b53ad40d28af0e02d8d18d70855f
LostCore <lostcore@gmail.com>
2018-04-10 17:27:32 +0200

    Standardized version

--- Header_Flex.php
+++ Header_Flex.php
@@ -4,7 +4,7 @@ Component Name: Header Flex
 Description: Header Flex Component
 Category: Layout
 Tags: Header, Flexbox
-Version: 1.0
+Version: 1.0.0
 Author: WAGA Team <dev@waga.it>
 Author URI: http://www.waga.it
 */
