8a135a703014717a75f1731f5f02403accfe3e71
fabosway <fabio@waga.it>
2018-04-24 17:47:13 +0200

    Fix Header Flex Logo on Mobile

--- templates/header-content.php
+++ templates/header-content.php
@@ -1,13 +1,15 @@
 <div class="header__logo">
     <a href="<?php echo home_url( '/' ); ?>">
         <?php if ( \Waboot\template_tags\get_desktop_logo() != "" ) : ?>
-            <?php \Waboot\template_tags\desktop_logo(); ?>
+            <?php \Waboot\template_tags\desktop_logo(false, 'waboot-desktop-logo'); ?>
         <?php else : ?>
             <?php \Waboot\template_tags\site_title(); ?>
         <?php endif; ?>
 
         <?php if ( \Waboot\template_tags\get_mobile_logo() != "" ) : ?>
-            <?php \Waboot\template_tags\mobile_logo() ?>
-        <?php  endif; ?>
+            <?php \Waboot\template_tags\mobile_logo(false, 'waboot-mobile-logo') ?>
+        <?php else : ?>
+            <?php \Waboot\template_tags\desktop_logo(false, 'waboot-mobile-logo'); ?>
+        <?php endif; ?>
     </a>
 </div>
